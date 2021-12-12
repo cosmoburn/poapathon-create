fe4b1fb6d99dfb0249447b576d35dfc90ef30453
Arthur Silveira <arthur.saboya@gmail.com>
2021-12-12 11:11:37 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "POAPathon Create",
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+};
